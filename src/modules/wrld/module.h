e3f489421878d5c911adf9989061bf4c0a77bb4d
wk1093 <wyattk1093@gmail.com>
2025-02-23 10:35:47 -0700

    ideas

--- src/modules/wrld/module.h
+++ src/modules/wrld/module.h
@@ -51,6 +51,76 @@ File structure of compiled game:
 most structs in the engine are prefixed with "s" (for Spectral), but this module is prefixed with "sw" (for Spectral World)
 */
 
+/*
+The above comment is outdated, as I have decided to make some changes as to how the game will be stored
+The game (on the developer's side) will be in a folder, with basically the same structure as the above specs
+But when the game is compiled, much of this data will be converted into C++ code and compiled into dlls
+Each level/scene will be a separate dll, and the user scripts will all be compiled into a single dll
+The main change, is that a level, asset, or script can be marked as a "moddable" asset, and it will not be compiled into anything, it will be stored in the final game folder under a user/ folder
+The engine will also make a mods/ folder, where the user can place mods which will contain replacements for the assets in the moddable folder. The engine will hash every dll (modules, user scripts, and levels) as well as the moddable scripts. and will prevent the game from running if any of these files are modified
+When the engine loads (on computers only, consoles do not support mods and selection of api), it will open up a popup letting the user select which modules to use, and mods to enable (if any)
+This will allow the user to select things like OpenGL or DirectX. and will allow them to add custom modules. For example: If the user wanted to use vulkan, and someone made a vulkan module, they could add the vulkan module to the modules folder, and select it in the engine settings (this will give a warning that the module is not official, and could be dangerous)
+The engine will never select a non-official module by default, and will always open the config popup if the user doesn't click "don't show this again"
+If the user selects a non-official module, and then "don't show this again", it will still be shown to give the user a warning that they are using a non-official module
+This popup menu is completely seperate from the rest of the engine, and will be run before anything else in the engine is loaded.
+So the engine can recognize official modules, when the engine is compiled, it will hash all the official modules, and the hash will be stored inside of the engine executable. When the engine is run, it will verify that the official modules have not been modified, and if they have, it will act as if that module is non-official
+The engine executable and official modules will all be the same for every game, since the game is actually just a few dlls.
+
+All assets (non-moddable) will be compiled into an asset dll, which will store them as C++ code (just an array of bytes), and will be loaded into the game when the game starts
+The assets that are moddable will be in the user/ folder.
+
+This part of the engine (runtime) has no idea how to deal with the json format of the game, only the compiled format
+
+The engine is split into 3 parts:
+- Runtime: The part of the engine that runs the game. This contains all the modules, and the main executable. This part is compiled into the engine exe, and the module dlls
+- Editor: This is the last part that I will make, and it will be used to create and edit the game file (in the dev format json). This part will be a completely separate program, but will borrow some of the modules from the runtime
+- Compiler: This part is used to compile the game into the runtime format. This part will integrate with CMake, and will be used to make a game project into a game dll. This part will be put in the game/ directory of the output from the first part.
+
+
+So a final compiled game will look like this:ABC
+- game/
+  - assets.dll
+  - levels/
+    - level1.dll
+    - level2.dll
+  - scripts.dll
+- modules/
+  - all the modules needed to run the game
+- user/
+  - assets/
+    - moddable assets
+  - levels/
+    - moddable levels
+  - scripts/
+    - moddable scripts
+- mods/
+    - mod1/
+        - assets/
+        - moddable assets
+        - levels/
+        - moddable levels
+        - scripts/
+        - moddable scripts
+    - mod2/
+        - assets/
+        - moddable assets
+        - levels/
+        - moddable levels
+        - scripts/
+        - moddable scripts
+- Spectral.PLATFORM.exe
+- data/
+    - logs/
+    - save/
+    - settings/
+- lib/
+    - all the libraries needed to run the game
+- engine/
+    - engine settings
+- cache/
+    - cached data
+*/
+
 #pragma once
 
 #include "../moduleLib.h"
