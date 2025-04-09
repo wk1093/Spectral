3c160c592ae5f5fa6525ef124fd27e9ed19f6b08
wk1093 <wyattk1093@gmail.com>
2025-04-09 13:01:45 -0600

    network plan

---
+++ src/modules/net/module.h
@@ -0,0 +1,6 @@
+/**
+ * @file net/module.h
+ * @brief Network module header file
+ * @details This file implements very basic, but cross-platform network functionality.
+ * There will be a more advanced "server" module in the future that will use this module. It will implement mutliplayer functionality in an easy to use API.
+ */
